1396cb729212aa2227379b55fb7f234e0e654336
RCRR <young2846@hotmail.com>
2020-06-28 12:07:25 -0500

    Se concluyó la parte pendiente de los tipos de hardware. Se finalizó totalmente la parte relacionada al  hardware

--- src/main/java/com/linking/pc/controllers/hardwareController.java
+++ src/main/java/com/linking/pc/controllers/hardwareController.java
@@ -1,6 +1,7 @@
 package com.linking.pc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,5 +14,27 @@ public class hardwareController {
 	public ModelAndView verHardware(ModelAndView mav) {
 		mav.setViewName("hardware/verHardware");
 		return mav;
+	}
+	
+	@GetMapping({"/entrada","/entrada/"})
+	public String hardwareEntrada(Model model) {
+		return "hardware/verHardwareEntrada";
+	}
+	
+	@GetMapping({"/proceso","/proceso/"})
+	public String hardwareProceso(Model model) {
+		return "hardware/verHardwareProceso";
+	}
+	
+	@GetMapping({"/salida","/salida/"})
+	public String hardwareSalida(Model model) {
+		model.addAttribute("titulo", "Hardware de salida");
+		return "hardware/verHardwareSalida";
+	}
+	
+	@GetMapping({"/almacenamiento","/almacenamiento/"})
+	public String hardwareAlmacenamiento(Model model) {
+		model.addAttribute("titulo", "Hardware de almacenamiento");
+		return "hardware/verHardwareAlmacenamiento";
 	}
 }
